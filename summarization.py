f64f5c41e00e975d7bde4b8736b2bec80616c366
wsy8029 <wsy8029@gmail.com>
2021-09-02 17:59:49 +0900

    add label and stringvar

--- summarization.py
+++ summarization.py
@@ -2,42 +2,41 @@ from tkinter import *
 from tkinter.ttk import *
 from PIL import Image, ImageTk
 
-# win = Tk()
-# win.geometry("1200x550+100+100")
-# win.title("LUXROBO 자기소개서 요약 시스템 v0.1")
+class MyFrame():
+    def __init__(self):
+        self.win = Tk()
+        self.win.geometry("1200x550+100+100")
+        self.win.title("LUXROBO 자기소개서 요약 시스템 v0.1")
+        self.frame = Frame(self.win)
+        self.frame.pack(fill=X)
+        # self.canvas = Canvas(self.frame, bd=0, width=500, height=120)
+        self.canvas = Canvas(self.frame, bd=0, width=500, height=120)
+        self.canvas.pack(fill=X)
+        
+        # 로고 이미지 파일 리사이즈
+        self.logo_img= Image.open("logo.png")
+        self.resized_image= self.logo_img.resize((250,60), Image.ANTIALIAS)
+        self.new_logo= ImageTk.PhotoImage(self.resized_image)
+        self.canvas.create_image(600,60,anchor=CENTER, image=self.new_logo)
 
-# frame = Frame(win)
-# frame.pack()
-# canvas = Canvas(frame, bd=0, width=500, height=120)
-# canvas.pack()
 
-# # 로고 이미지 파일 리사이즈
-# logo_img= Image.open("logo.png")
-# resized_image= logo_img.resize((250,60), Image.ANTIALIAS)
-# new_logo= ImageTk.PhotoImage(resized_image)
-# canvas.create_image(250,50,anchor=CENTER, image=new_logo)
+        labeltext = Label(self.frame, text="원문 텍스트 입력", width=10)
+        labeltext.pack(side=LEFT, padx=10, pady=10)
+ 
+        self.inputURL = StringVar()
+        entryURL = Entry(self.frame, textvariable = self.inputURL)
+        entryURL.pack(fill=X, padx=10, expand=True)
 
-# win.mainloop()
 
-class MyFrame(Frame):
-    def __init__(self, master):
-        self.master = master
-        self.master.title("LUXROBO 자기소개서 요약 시스템 v0.1")
-        # self.pack(fill=BOTH, expand=True)
-        frame = Frame(self)
-        # frame.pack()
+
+        self.win.mainloop()
 
 
 
 def main():
-    root = Tk()
-    root.geometry("1200x550+100+100")
 
-    label = Label(root)
-    label.pack()
-
-    app = MyFrame(root)
-    root.mainloop()
+    app = MyFrame()
+    # root.mainloop()
 
 if __name__ == '__main__':
     main()
